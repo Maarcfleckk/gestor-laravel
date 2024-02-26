ce528616f3959720d187fb90083aaea79cc36aa7
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-02-26 15:46:42 +0100

    first commit

---
+++ database/migrations/2024_02_23_144614_create_productes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Crear la taula 'productes'
+        Schema::create('productes', function (Blueprint $table) {
+            $table->id(); // Camp autoincremental, clau primària
+            $table->string('nom'); // Camp de tipus cadena per al nom del producte
+            $table->text('descripcio'); // Camp de tipus text per a descripcions més llargues
+            $table->decimal('preu', 8, 2); // Camp de tipus decimal per al preu amb 8 dígits totals, 2 dígits decimals
+            $table->integer('estoc'); // Camp de tipus enter per a l'estoc del producte
+            // Altres camps necessaris...
+
+            $table->timestamps(); // Dos camps de data i hora per al seguiment de quan es va crear i actualitzar el registre
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Desfer la creació de la taula 'productes'
+        Schema::dropIfExists('productes');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-home.css">
+    <title>Gestor de Productes</title>
+</head>
+<body>
+    <h1>Gestor de Productes</h1>
+    <div class="container">
+        <div class="product">
+            <h2>Producte 1</h2>
+            <p>Descripció del producte 1</p>
+        </div>
+        <div class="product">
+            <h2>Producte 2</h2>
+            <p>Descripció del producte 2</p>
+        </div>
+        <div class="product">
+            <h2>Producte 3</h2>
+            <p>Descripció del producte 3</p>
+        </div>
+    </div>
+</body>
+</html>
