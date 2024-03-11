8f31047c80529d9d76e444d99a91c91d574558d0
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-03-11 17:34:26 +0100

    push

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,37 +8,46 @@
     <link rel="icon" href="{{ asset('images/favicon.png')}}" type="image/x-icon">
     <title>Document</title>
 </head>
+
 <body>
-<header>
-    <div class="menu">
-      <div class="title">Gestor de Productos</div>
-      <div class="menu-items">
-        <ul>
-          <li><a href="#">Tienda</a></li>
-          <li><a href="#">Filtrar</a></li>
-          <li><a href="#">Crear</a></li>
-        </ul>
-      </div>
-    </div>
-    <hr>
-  </header>
-  <section class="product-section">
-  @foreach ($productes as $producte)
-    <div id="item{{$producte->id}}" class="product">
-      <h2>{{ $producte->nom }}</h2>
-      <p>{{ $producte->descripcio }}</p>
-      <p>{{ $producte->preu }} €</p>
-      <p>Estoc: {{ $producte->estoc }}</p>
-      <a href="productes/?{{ $producte }}">Veure més</a>
-      <a href="">Editar</a>
-      <form method="POST" action="{{ route('productes.destroy', ['id' => $producte->id]) }}" style="display: inline;">
-        @csrf
-        @method('DELETE')
-        <button type="submit" onclick="return confirm('¿Estás seguro de eliminar este producto?')">Eliminar</button>
-      </form>
-    </div>
-  @endforeach
-</section>
+    <header>
+        <div class="menu">
+            <div class="title">Gestor de Productos</div>
+            <div class="menu-items">
+                <ul>
+                    <li><a href="#">Tienda</a></li>
+                    <li><a href="#">Crear</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </header>
+    <section class="product-section">
+        @foreach ($productes as $producte)
+        <div id="item{{$producte->id}}" class="product">
+            <h2 class="nom">{{ $producte->nom }}</h2>
+            <p class="desc">{{ $producte->descripcio }}</p>
+            <p class="preu">{{ $producte->preu }} €</p>
+            <p class="estock">Estoc: {{ $producte->estoc }}</p>
+            <div class="buttonContainer">
+                <a class="veure" href="productes/?{{ $producte }}">
+                    <img src="{{ asset('/images/eye-solid.svg')}}" alt="Icono de ojo">Veure més
+                </a>
+                <a class="editar" href="">
+                    <img src="{{ asset('/images/pencil-solid.svg')}}" alt="Icono de lapiz">Editar
+                </a>
+                <form method="POST" action="{{ route('productes.destroy', ['id' => $producte->id]) }}" style="display: inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="eliminar" onclick="return confirm('Segur que vols eliminar aquest producte?')">
+                        <img src="{{ asset('/images/trash-regular.svg')}}" alt="Icono de lapiz">Eliminar
+                    </button>
+                </form>
+            </div>
+        </div>
+        @endforeach
+    </section>
     <a href="{{ asset('css/app.css')}}"></a>
 </body>
+
 </html>
