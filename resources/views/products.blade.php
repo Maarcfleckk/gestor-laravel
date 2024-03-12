2ec644d9d9e4ac37c4a42690999e0405bd8006e2
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-03-12 16:48:50 +0100

    h

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -27,13 +27,13 @@
         <div id="item{{$producte->id}}" class="product">
             <h2 class="nom">{{ $producte->nom }}</h2>
             <p class="desc">{{ $producte->descripcio }}</p>
-            <p class="preu">{{ $producte->preu }} €</p>
+            <p class="preu">Preu: {{ $producte->preu }} €</p>
             <p class="estock">Estoc: {{ $producte->estoc }}</p>
             <div class="buttonContainer">
-              <a class="veure" href="{{ route('productes.show', ['producte' => $producte->id]) }}">
-                <img src="{{ asset('/images/eye-solid.svg')}}" alt="Icono de ojo">Veure més
-              </a>
-              <a class="editar" href="{{ route('productes.edit', ['id' => $producte->id]) }}">
+                <a class="veure" href="{{ route('productes.show', ['producte' => $producte->id]) }}">
+                    <img src="{{ asset('/images/eye-solid.svg')}}" alt="Icono de ojo">Veure més
+                </a>
+                <a class="editar" href="{{ route('productes.edit', ['id' => $producte->id]) }}">
                     <img src="{{ asset('/images/pencil-solid.svg')}}" alt="Icono de lapiz">Editar
                 </a>
                 <form method="POST" action="{{ route('productes.destroy', ['id' => $producte->id]) }}" style="display: inline;">
