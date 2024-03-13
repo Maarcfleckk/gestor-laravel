a6d84b4254e6882e6ee932a3a6885ddc5a88825d
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-03-13 18:54:40 +0100

    push

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -31,18 +31,18 @@
                 <p class="preu">Preu: {{ $producte->preu }} €</p>
                 <p class="estock">Estoc: {{ $producte->estoc }}</p>
                 <form method="POST" action="{{ route('productes.destroy', ['id' => $producte->id]) }}" class="formBottons">
-            <a class="veure" href="{{ route('productes.show', ['producte' => $producte->id]) }}">
-                <img src="{{ asset('/images/eye-solid.svg')}}" alt="Icono de ojo">Veure més
-            </a>
-            <a class="editar" href="{{ route('productes.edit', ['id' => $producte->id]) }}">
-                <img src="{{ asset('/images/pencil-solid.svg')}}" alt="Icono de lapiz">Editar
-            </a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="eliminar" onclick="return confirm('Segur que vols eliminar aquest producte?')">
-                    <img src="{{ asset('/images/trash-regular.svg')}}" alt="Icono de lapiz">Eliminar
-                </button>
-            </form>
+                    <a class="veure" href="{{ route('productes.show', ['producte' => $producte->id]) }}">
+                        <img src="{{ asset('/images/eye-solid.svg')}}" alt="Icono de ojo">Veure més
+                    </a>
+                    <a class="editar" href="{{ route('productes.edit', ['id' => $producte->id]) }}">
+                        <img src="{{ asset('/images/pencil-solid.svg')}}" alt="Icono de lapiz">Editar
+                    </a>
+                        @csrf
+                        @method('DELETE')
+                    <button type="submit" class="eliminar" onclick="return confirm('Segur que vols eliminar aquest producte?')">
+                        <img src="{{ asset('/images/trash-regular.svg')}}" alt="Icono de lapiz">Eliminar
+                    </button>
+                </form>
             </div>
             <div class="product-image">
                 <img src="{{ asset('storage/'.$producte->imagen.'') }}" alt="Imagen del producto">
